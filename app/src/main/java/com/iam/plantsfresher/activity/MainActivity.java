73d1470f4ffb7184586833a961c928ca8584ceec
Akash Yadav <ay790184@gmail.com>
2025-05-28 23:32:44 +0530

    manage views

--- app/src/main/java/com/iam/plantsfresher/activity/MainActivity.java
+++ app/src/main/java/com/iam/plantsfresher/activity/MainActivity.java
@@ -1,29 +1,142 @@
 package com.iam.plantsfresher.activity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.iam.plantsfresher.R;
+import com.iam.plantsfresher.fragment.HangFragment;
+import com.iam.plantsfresher.fragment.HomeFragment;
+import com.iam.plantsfresher.fragment.ProfileFragment;
+import com.iam.plantsfresher.fragment.WishlistFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavigationView navigationView;
+    ImageView imgMenu;
+    DrawerLayout drawerLayout;
+    int currentBottomNavItemId = R.id.bottom_home; // Track only bottom nav items
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize views
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navigationView = findViewById(R.id.navigationView);
+        imgMenu = findViewById(R.id.imgMenu);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        // Cart button
+        ImageView cart = findViewById(R.id.cart);
+        cart.setOnClickListener(v -> {
+            // Handle cart click
         });
 
+        // Drawer toggle
+        imgMenu.setOnClickListener(v -> {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
+        // Load HomeFragment by default
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+            currentBottomNavItemId = R.id.bottom_home;
+        }
 
+        // Bottom navigation
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            currentBottomNavItemId = item.getItemId(); // Update current bottom nav item
+            return handleNavigation(item.getItemId());
+        });
+
+        // Side navigation
+        navigationView.setNavigationItemSelectedListener(item -> handleNavigation(item.getItemId()));
+    }
+
+    private boolean handleNavigation(int itemId) {
+        Fragment selectedFragment = null;
+
+        // Close drawer if it's open
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        if (itemId == R.id.bottom_home || itemId == R.id.side_home) {
+            selectedFragment = new HomeFragment();
+            if (itemId == R.id.side_home) {
+                bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+                currentBottomNavItemId = R.id.bottom_home;
+            }
+        } else if (itemId == R.id.bottom_wishlist || itemId == R.id.side_wishlist) {
+            selectedFragment = new WishlistFragment();
+            if (itemId == R.id.side_wishlist) {
+                bottomNavigationView.setSelectedItemId(R.id.bottom_wishlist);
+                currentBottomNavItemId = R.id.bottom_wishlist;
+            }
+        } else if (itemId == R.id.bottom_hang || itemId == R.id.side_hang) {
+            selectedFragment = new HangFragment();
+            if (itemId == R.id.side_hang) {
+                bottomNavigationView.setSelectedItemId(R.id.bottom_hang);
+                currentBottomNavItemId = R.id.bottom_hang;
+            }
+        } else if (itemId == R.id.bottom_profile || itemId == R.id.side_profile) {
+            selectedFragment = new ProfileFragment();
+            if (itemId == R.id.side_profile) {
+                bottomNavigationView.setSelectedItemId(R.id.bottom_profile);
+                currentBottomNavItemId = R.id.bottom_profile;
+            }
+        }
+
+        if (selectedFragment != null) {
+            loadFragment(selectedFragment);
+            return true;
+        }
+        return false;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(
+                        R.anim.slide_in,
+                        R.anim.fade_out,
+                        R.anim.fade_in,
+                        R.anim.slide_out
+                )
+                .replace(R.id.nav_fragment_host, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (currentBottomNavItemId != R.id.bottom_home) {
+            // Navigate to home and update bottom navigation
+            bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+            currentBottomNavItemId = R.id.bottom_home;
+            loadFragment(new HomeFragment());
+        } else {
+            super.onBackPressed();
+        }
     }
 }
